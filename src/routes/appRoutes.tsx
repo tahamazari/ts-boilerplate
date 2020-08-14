78a829e41fbd086142cf98024f94c1925f72b25a
tahamazari <tahamazari@gmail.com>
2020-08-14 23:17:47 +0500

    deleted useless App component

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router';
 
-import { App } from "../components/App";
 import First from "../containers/First"
 import Second from "../containers/Second";
 import Third from "../components/Pages/Third";
@@ -11,6 +10,5 @@ export default (
         <Route exact path="/first" component={First}/>
         <Route exact path="/second" component={Second}/>
         <Route exact path="/third" component={Third}/>
-        <Route exact path="/" component={App}/>
     </Switch>
 );
